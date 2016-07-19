d3b3a05fe83764cc1ebd862c0bfec777ef42f09f
Andrew Hutton <fromalaska49@gmail.com>
2016-07-18 23:23:31 -0500

    Added the .sh extension
    This is just a temporary stopgap solution to get the proper syntax
    highlighting while editing the file

--- ibash.config.sh
+++ ibash.config.sh
@@ -1,3 +1,12 @@
 . "${BASH_SOURCE%/*}/module_loader.sh"
 
+
 load_ibash_module "$ibash_path/definitions/text.sh"
+load_ibash_module "$ibash_path/definitions/background.sh"
+load_ibash_module "$ibash_path/definitions/videopaper.sh"
+load_ibash_module "$ibash_path/definitions/cd_up.sh"
+load_ibash_module "$ibash_path/definitions/prompt.sh"
+load_ibash_module "$ibash_path/definitions/help.sh"
+load_ibash_module "$ibash_path/definitions/extract.sh"
+load_ibash_module "$ibash_path/definitions/fml.sh"
+load_ibash_module "$ibash_path/definitions/play.sh"
