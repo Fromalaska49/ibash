47545cd7e7a416dccdf6b86fb25eda30697e7510
Andrew Hutton <fromalaska49@gmail.com>
2016-07-18 23:25:21 -0500

    added a function to set screensaver as background

---
+++ definitions/videopaper.sh
@@ -0,0 +1,5 @@
+videopaper() {
+  nohup /System/Library/Frameworks/ScreenSaver.framework/Resources/ScreenSaverEngine.app/Contents/MacOS/ScreenSaverEngine -background &
+#  cd /System/Library/Frameworks/ScreenSaver.framework/Resources --silent
+#  ./ScreenSaverEngine.app/Contents/MacOS/ScreenSaverEngine -background >/dev/null
+}
