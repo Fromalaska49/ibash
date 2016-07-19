c585127d6ff7bde84ef50bf02f48a2f029669f5a
Andrew Hutton <fromalaska49@gmail.com>
2016-07-18 23:48:43 -0500

    Rebased the logic

--- definitions/fml.sh
+++ definitions/fml.sh
@@ -1,7 +1,8 @@
-fml() {
+provide_emotional_support() {
   printf "Remember, when you hit the bottom, you'll have no where to go but up.\n"
 }
 
-alias damn='fml'
-alias shit='fml'
-alias crap='fml'
+alias fml='provide_emotional_support'
+alias damn='provide_emotional_support'
+alias shit='provide_emotional_support'
+alias crap='provide_emotional_support'
