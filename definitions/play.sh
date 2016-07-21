5db7dd5e410ac395df894316c4f88500cb3c82aa
Andrew Hutton <fromalaska49@gmail.com>
2016-07-21 00:04:26 -0500

    Expanded compatibility

--- definitions/play.sh
+++ definitions/play.sh
@@ -1,36 +1,48 @@
 play() {
-  if [ "$1" == "star wars" ]; then
-    telnet towel.blinkenlights.nl
-  elif [ "$1" == "tetris" ]
-  then
-    emacs -f tetris
-  elif [ "$1" == "snake" ]
-  then
-    emacs -f snake
-  elif [ "$1" == "life" ]
-  then
-    emacs -f life
-  elif [ "$1" == "gomoku" ]
-  then
-    emacs -f gomoku
-  elif [ "$1" == "adventure" ]
-  then
-    emacs -f adventure
-  elif [ "$1" == "zone out" ]; then
-    emacs -f "zone out"
-  elif [ "$1" == "solitaire" ]; then
-    emacs -f solitaire
-  elif [ "$1" == "multiplication puzzle" ]; then
-    emacs -f "multiplication puzzle"
-  elif [ "$1" == "towers of hanoi" ]; then
-    emacs -f "towers of hanoi"
-  elif [ "$1" == "blackbox" ]; then
-    emacs -f "blackbox"
-  elif [ "$1" == "5x5" ]; then
-    emacs -f "5x5"
-  elif [ "$1" == "something" ]; then
-    printf "What would you like to play?\n"
+  if [ "$#" -gt "0" ]; then
+    arg_1=`echo "$1" | tr '[:upper:]' '[:lower:]'`
+    if [ "$#" -gt "1" ]; then
+      printf "Error: 1 argument expected, but $# were received\n"
+    else
+      if [ "$arg_1" == "star wars" ]; then
+        telnet towel.blinkenlights.nl
+      elif [ "$arg_1" == "tetris" ]
+      then
+        emacs -f tetris
+      elif [ "$arg_1" == "snake" ]
+      then
+        emacs -f snake
+      elif [ "$arg_1" == "life" ]
+      then
+        emacs -f life
+      elif [ "$arg_1" == "gomoku" ]
+      then
+        emacs -f gomoku
+      elif [ "$arg_1" == "adventure" ]
+      then
+        emacs -f adventure
+      elif [ "$arg_1" == "zone out" ]; then
+        emacs -f "zone out"
+      elif [ "$arg_1" == "solitaire" ]; then
+        emacs -f solitaire
+      elif [ "$arg_1" == "multiplication puzzle" ]; then
+        emacs -f "multiplication puzzle"
+      elif [ "$arg_1" == "towers of hanoi" ]; then
+        emacs -f "towers of hanoi"
+      elif [ "$arg_1" == "blackbox" ]; then
+        emacs -f "blackbox"
+      elif [ "$arg_1" == "5x5" ]; then
+        emacs -f "5x5"
+      elif [ "$arg_1" == "something" ]; then
+        printf "What would you like to play?\n"
+      else
+        printf "unrecognized argument '$1'\n"
+      fi
+    fi
   else
-    printf "unrecognized argument '$1'\n"
+    printf "I can't read your mind. Tell me what you want to play.\n"
+    #read -r
+    #printf "play ${REPLY}\n"
+    #`play "$REPLY"`
   fi
 }
