549593337795b544cae35afccc2e38492058cd33
Andrew Hutton <fromalaska49@gmail.com>
2016-08-11 09:51:32 -0500

    Created File for config()

--- ibash.config.sh
+++ ibash.config.sh
@@ -2,18 +2,20 @@
 
 #set showHiddenFiles here
 
-load_ibash_module "$ibash_path/definitions/text.sh"
-load_ibash_module "$ibash_path/definitions/background.sh"
-load_ibash_module "$ibash_path/definitions/videopaper.sh"
-load_ibash_module "$ibash_path/definitions/cd_up.sh"
-load_ibash_module "$ibash_path/definitions/prompt.sh"
-load_ibash_module "$ibash_path/definitions/help.sh"
-load_ibash_module "$ibash_path/definitions/extract.sh"
-load_ibash_module "$ibash_path/definitions/fml.sh"
-load_ibash_module "$ibash_path/definitions/play.sh"
-load_ibash_module "$ibash_path/definitions/verbose.sh"
-load_ibash_module "$ibash_path/definitions/cd.sh"
-load_ibash_module "$ibash_path/definitions/ls.sh"
-load_ibash_module "$ibash_path/definitions/ssh.sh"
+load_ibash_module "${ibash_path}/definitions/text.sh"
+load_ibash_module "${ibash_path}/definitions/background.sh"
+load_ibash_module "${ibash_path}/definitions/videopaper.sh"
+load_ibash_module "${ibash_path}/definitions/cd_up.sh"
+load_ibash_module "${ibash_path}/definitions/prompt.sh"
+load_ibash_module "${ibash_path}/definitions/help.sh"
+load_ibash_module "${ibash_path}/definitions/extract.sh"
+load_ibash_module "${ibash_path}/definitions/fml.sh"
+load_ibash_module "${ibash_path}/definitions/play.sh"
+load_ibash_module "${ibash_path}/definitions/verbose.sh"
+load_ibash_module "${ibash_path}/definitions/cd.sh"
+load_ibash_module "${ibash_path}/definitions/ls.sh"
+load_ibash_module "${ibash_path}/definitions/ssh.sh"
 
+#load_ibash_module "${ibash_path}/definitions/conf.sh"
+#load_ibash_module "${ibash_path}/definitions/ibash.sh"
 
